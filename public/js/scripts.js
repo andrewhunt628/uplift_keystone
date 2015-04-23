610a855752d734feff1e659c8f5fdf546f406531
Michael Blakeney <mike@acts18tech.com>
2015-04-23 08:05:56 -0800

    triggering post to endpoint

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -8,4 +8,24 @@ $(document).ready(function() {
     event.preventDefault();
     location.href=$(this).data('href');
   });
+
+
+  /* Accepts Enquiry from the enquiry modal */
+  $('button#enquiry-form').click(function(){
+    console.log('submitting form');
+    $.ajax({
+      type: 'POST',
+      url: '/enquiry',
+      data: $('form.contact').serialize(),
+      success: function(msg) {
+        alert('Thanks!' + msg);
+      },
+      error: function() {
+        alert('Sorry, something went wrong while trying to send the enquiry');
+      }
+    });
+
+  });
+
+
 });
